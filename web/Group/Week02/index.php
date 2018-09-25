f296f347b23ca6d5c76c3e68f37276eec9eb4d73
Rick Johnson <joh12155@byui.edu>
2018-09-25 18:38:20 -0400

    added week 02 group assignment

---
+++ web/Group/Week02/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Week 02 Group Assignment</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="./css/Week02.css" rel="stylesheet" />
+  <script src="./js/Week02.js"></script>
+</head>
+<body>
+<form>
+<div class="container-fluid">
+  <h1>Week 02 Group Assignment</h1>
+  <div class="row-fluid">
+  
+  <div id="div1" class="contentDiv span6"> 
+	  
+	  <div class="form-group">
+		  <label class="control-label col-sm-2" for="color">Color:</label>
+		  <div class="col-sm-10">          
+			<input type="text" id="color" name="color" value="" placeholder="Enter Color" />
+		  </div>			  
+			  <p>This is the first div</p>
+	  </div>
+  </div>
+  
+  <div id="div2" class="contentDiv span6">
+	<p>This is the second div</p>
+</div>
+  
+  <div id="div3" class="contentDiv span6">
+	<p>This is the thrid div<p>
+  </div>
+  <br>
+</div>
+  <button id="btnClickMe" type="button" class="btn">Click Me</button>
+  <button id="btnChangeColor" type="button" class="btn">Change Color</button>
+  <button id="btn3" type="button" class="btn">Hide</button>
+</div>
+</form>
+</body>
+</html> 
---
+++ web/Group/Week02/js/Week02.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    $("#btnClickMe").button().click(function () {
+        alert("You have cliked the click me button!");
+    });
+
+    $("#btnChangeColor").button().click(function () {
+        $("#colorpicker").addClass("hidden");
+        var newColor = $("#color").val();
+        $('#div1').css('background-color', newColor);
+    });
+
+    $("#btn3").button().click(function () {
+        if ($(this).html() == "Hide") {
+            $(this).html("Show");
+            $("#title3").fadeOut(3000);
+            $("#div3").fadeOut(3000);
+        }
+        else {
+            $(this).html("Hide");
+            $("#title3").fadeIn(3000);
+            $("#div3").fadeIn(3000);
+        }
+    });
+});
+
+
+
+
