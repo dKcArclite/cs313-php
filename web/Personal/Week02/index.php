f75b353721cf29ef6ccc3fd9e338383c9161dd33
Rick Johnson <joh12155@byui.edu>
2018-09-27 22:24:57 -0400

    adding images

--- web/Personal/Week02/index.php
+++ web/Personal/Week02/index.php
@@ -31,21 +31,21 @@
 	<p></p>
 	<p class="note">Click on the small "i" icon in the top left of the image to view image description.</p>
 	<div class="galleria">
-		<img src="images/IMG_01.jpg" data-title="Image 1" data-description="I really should not have bought this car, what was I thinking."/>
-		<img src="images/IMG_02.jpg" data-title="Image 2" data-description="As you can see the car was a complete rusted mess."/>
-		<img src="images/IMG_03.jpg" data-title="Image 3" data-description="If you look closely you can see the holes through the floor pan"/>
-		<img src="images/IMG_04.jpg" data-title="Image 4" data-description="This was suppossedly a recently rebuild engine."/>
-		<img src="images/IMG_05.jpg" data-title="Image 5" data-description="An old Holley 650 cfm 4 barrel carb"/>
-		<img src="images/IMG_06.jpg" data-title="Image 6" data-description="But at least the oil filter looked new :)"/>
-		<img src="images/IMG_07.jpg" data-title="Image 7" data-description="I was in way over my head, but look at what happens if you stick with it"/>	
-		<img src="images/IMG_08.jpg" data-title="Image 8" data-description="Starting to look different in the interior of the car"/>	
-		<img src="images/IMG_09.jpg" data-title="Image 9" data-description="Front Seats"/>
-		<img src="images/IMG_10.jpg" data-title="Image 10" data-description="Rear Seats"/>
-		<img src="images/IMG_11.jpg" data-title="Image 11" data-description="New 302 Crate Engine 372 HP"/>
-		<img src="images/IMG_12.jpg" data-title="Image 12" data-description="Sitting pretty in my driveway"/>
-		<img src="images/IMG_13.jpg" data-title="Image 13" data-description="I love the rims"/>
-		<img src="images/IMG_14.jpg" data-title="Image 14" data-description="Not so sure about the hood scoop"/>
-		<img src="images/IMG_15.jpg" data-title="Image 15" data-description="Awesome"/>	
+		<img src="images/IMG01.jpg" data-title="Image 1" data-description="I really should not have bought this car, what was I thinking."/>
+		<img src="images/IMG02.jpg" data-title="Image 2" data-description="As you can see the car was a complete rusted mess."/>
+		<img src="images/IMG03.jpg" data-title="Image 3" data-description="If you look closely you can see the holes through the floor pan"/>
+		<img src="images/IMG04.jpg" data-title="Image 4" data-description="This was suppossedly a recently rebuild engine."/>
+		<img src="images/IMG05.jpg" data-title="Image 5" data-description="An old Holley 650 cfm 4 barrel carb"/>
+		<img src="images/IMG06.jpg" data-title="Image 6" data-description="But at least the oil filter looked new :)"/>
+		<img src="images/IMG07.jpg" data-title="Image 7" data-description="I was in way over my head, but look at what happens if you stick with it"/>	
+		<img src="images/IMG08.jpg" data-title="Image 8" data-description="Starting to look different in the interior of the car"/>	
+		<img src="images/IMG09.jpg" data-title="Image 9" data-description="Front Seats"/>
+		<img src="images/IMG10.jpg" data-title="Image 10" data-description="Rear Seats"/>
+		<img src="images/IMG11.jpg" data-title="Image 11" data-description="New 302 Crate Engine 372 HP"/>
+		<img src="images/IMG12.jpg" data-title="Image 12" data-description="Sitting pretty in my driveway"/>
+		<img src="images/IMG13.jpg" data-title="Image 13" data-description="I love the rims"/>
+		<img src="images/IMG14.jpg" data-title="Image 14" data-description="Not so sure about the hood scoop"/>
+		<img src="images/IMG15.jpg" data-title="Image 15" data-description="Awesome"/>	
 		<script>
 				Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
 				Galleria.run('.galleria');
